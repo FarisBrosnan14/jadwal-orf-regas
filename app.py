491985837845930f2f8e2415e2be316584e07a5d
FarisBrosnan14 <farisarsip14@gmail.com>
2026-04-20 10:11:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from PIL import Image
 # 1. KONFIGURASI UTAMA
 # =====================================================================
 try:
-    favicon = Image.open("logo-pertaminregasv2a.png")
+    favicon = Image.open("logo-pertaminregasv2.png")
 except:
     favicon = "⚡"
 
